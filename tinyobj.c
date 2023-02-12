22fda315d6ee6cd0a592266c790bf040b0ac0cb1
wivl <68458084+wivl@users.noreply.github.com>
2023-02-13 01:38:21 +0800

    Add uv and norm and texture support

--- tinyobj.c
+++ tinyobj.c
@@ -1,10 +1,13 @@
 #include "tinyobj.h"
 #include "geometry.h"
+#include "tinytga.h"
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
 
 #define DEFAULT_CHUNK_SIZE 100
 
@@ -13,7 +16,7 @@ void _dev_log(tobj_model *model);
 
 
 
-tobj_model* tobj_load_model(const char *filename) {
+tobj_model* tobj_load_model(const char *filename, bool has_texture) {
 	tobj_model* model = NULL;
 	FILE *file = fopen(filename, "r");
 	if (file == NULL) {
@@ -23,13 +26,24 @@ tobj_model* tobj_load_model(const char *filename) {
 
 	model = (tobj_model*)malloc(sizeof(tobj_model));
 	model->verts = (Vec3f*)malloc(sizeof(Vec3f)*DEFAULT_CHUNK_SIZE);
-	model->faces = (int **)malloc(sizeof(int*)*DEFAULT_CHUNK_SIZE);
+	model->faces = (Vec3i **)malloc(sizeof(int*)*DEFAULT_CHUNK_SIZE);
+	model->uv = (Vec2f*)malloc(sizeof(Vec2f)*DEFAULT_CHUNK_SIZE);
+	model->norms = (Vec3f*)malloc(sizeof(Vec3f)*DEFAULT_CHUNK_SIZE);
+
 	model->v_capp = 0;
 	model->f_capp = 0;
+	model->u_capp = 0;
+	model->n_capp = 0;
+
 	size_t v_chunk_count = 1;
 	size_t f_chunk_count = 1;
+	size_t u_chunk_count = 1;
+	size_t n_chunk_count = 1;
+
 	size_t v_index = 0;
 	size_t f_index = 0;
+	size_t u_index = 0;
+	size_t n_index = 0;
 
 	char* line_buf = NULL;
 	size_t line_buf_size = 0;
@@ -63,53 +77,97 @@ tobj_model* tobj_load_model(const char *filename) {
 			ptr += 2;
 			char *end;
 
-			int *face = (int*)malloc(sizeof(int)*3);
+			Vec3i *face = (Vec3i*)malloc(sizeof(Vec3i)*3);
 
-			long l;
-			l = strtof(ptr, &end); ptr = end+1;
-			face[0] = l;
+			face[0].x = strtof(ptr, &end); ptr = end+1;
 			// printf("[DEV]f %ld/", l);
-			l = strtof(ptr, &end); ptr = end+1;
+			face[0].y = strtof(ptr, &end); ptr = end+1;
 			// printf("%ld/", l);
-			l = strtof(ptr, &end); ptr = end+1;
+			face[0].z = strtof(ptr, &end); ptr = end+1;
 			// printf("%ld ", l);
 
 
-			l = strtof(ptr, &end); ptr = end+1;
-			face[1] = l;
+			face[1].x = strtof(ptr, &end); ptr = end+1;
 			// printf("%ld/", l);
-			l = strtof(ptr, &end); ptr = end+1;
+			face[1].y = strtof(ptr, &end); ptr = end+1;
 			// printf("%ld/", l);
-			l = strtof(ptr, &end); ptr = end+1;
+			face[1].z = strtof(ptr, &end); ptr = end+1;
 			// printf("%ld ", l);
 
 
-			l = strtof(ptr, &end); ptr = end+1;
-			face[2] = l;
+			face[2].x = strtof(ptr, &end); ptr = end+1;
 			// printf("%ld/", l);
-			l = strtof(ptr, &end); ptr = end+1;
+			face[2].y = strtof(ptr, &end); ptr = end+1;
 			// printf("%ld/", l);
-			l = strtof(ptr, &end); ptr = end+1;
+			face[2].z = strtof(ptr, &end); ptr = end+1;
 			// printf("%ld\n", l);
 
-			model->faces[f_index] = face;
+			model->faces[f_index++] = face;
 
-			f_index++;
 
 			if (f_index >= f_chunk_count*DEFAULT_CHUNK_SIZE) {
 				f_chunk_count++;
-				model->faces = (int **)realloc(model->faces, sizeof(int*)*f_chunk_count*DEFAULT_CHUNK_SIZE);
+				model->faces = (Vec3i **)realloc(model->faces, sizeof(Vec3i*)*f_chunk_count*DEFAULT_CHUNK_SIZE);
 			}
-		}
+		} else if (strncmp(line_buf, "vn ", 3) == 0) {
+            char *ptr = line_buf;
+            ptr += 3;
+            char *end;
+            Vec3f n;
+            n.x = strtof(ptr, &end); ptr = end+1;
+            n.y = strtof(ptr, &end); ptr = end+1;
+            n.z = strtof(ptr, &end); ptr = end+1;
+
+            model->norms[n_index++] = n;
+			if (n_index >= n_chunk_count*DEFAULT_CHUNK_SIZE) {
+				n_chunk_count++;
+				model->norms = (Vec3f *)realloc(model->norms, sizeof(Vec3f)*n_chunk_count*DEFAULT_CHUNK_SIZE);
+			}
+
+        } else if (strncmp(line_buf, "vt ", 3) == 0) {
+            char *ptr = line_buf;
+            ptr += 3;
+            char *end;
+            Vec2f uv;
+            uv.x = strtof(ptr, &end); ptr = end+1;
+            uv.y = strtof(ptr, &end); ptr = end+1;
+
+            model->uv[u_index++] = uv;
+			if (u_index >= u_chunk_count*DEFAULT_CHUNK_SIZE) {
+				u_chunk_count++;
+				model->uv = (Vec2f *)realloc(model->uv, sizeof(Vec2f)*u_chunk_count*DEFAULT_CHUNK_SIZE);
+			}
+        }
 
 		line_size = getline(&line_buf, &line_buf_size, file);
 	}
 	model->v_capp = v_index;
 	model->f_capp = f_index;
-	_dev_log(model);
+	model->u_capp = u_index;
+	model->n_capp = n_index;
+	// _dev_log(model);
+
+    // texture
+    model->has_texture = has_texture;
 
 	fclose(file);
 	return model;
+}
+
+tt_color tobj_diffuse(tobj_model *model, Vec2i uv) {
+    assert(model != NULL);
+    assert(model->_diffuse_map != NULL);
+    // HACK:  not sure x or x-1 
+    tt_color color = tt_make_color(model->_diffuse_map->pixels[(uv.y-1)*model->_diffuse_map->width+uv.x-1]);
+    return color;
+}
+
+void tobj_load_texture(tobj_model *model, const char* filepath) {
+    if (model->has_texture == false) {
+        return ;
+    }
+    model->_diffuse_map = tt_load_from_file(filepath);
+    tt_flip_vertically(model->_diffuse_map);
 }
 
 void _dev_log(tobj_model *model) {
--- tinyobj.h
+++ tinyobj.h
@@ -2,15 +2,36 @@
 #define _TINY_OBJ_H
 
 #include "geometry.h"
+#include "tinytga.h"
 #include <stddef.h>
+#include <stdbool.h>
+
+enum has_texture {
+    NO_TEXTURE = 0,
+    TEXTURE_TGA = 1
+};
 
 typedef struct {
+
 	size_t v_capp;
 	size_t f_capp;
+	size_t n_capp;
+	size_t u_capp;
+
 	Vec3f *verts;
-	int **faces;
+	Vec3i **faces; // attention, this Vec3i means vertex/uv/normal
+    Vec3f *norms;
+    Vec2f *uv;
+    tt_image *_diffuse_map;
+
+    int has_texture;
+
 } tobj_model;
 
-tobj_model* tobj_load_model(const char *filename);
+tobj_model* tobj_load_model(const char *filename, bool has_texture);
+
+tt_color tobj_diffuse(tobj_model *model, Vec2i uv);
+
+void tobj_load_texture(tobj_model *model, const char* filepath);
 
 #endif
