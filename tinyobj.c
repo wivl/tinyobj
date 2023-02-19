478ad1e354d9b7f39f7b729d0216af0a1220f998
wivl <68458084+wivl@users.noreply.github.com>
2023-02-19 16:11:18 +0800

    f indexes now starts from 0

--- tinyobj.c
+++ tinyobj.c
@@ -76,28 +76,33 @@ tobj_model* tobj_load_model(const char *filename, bool has_texture) {
 
 			Vec3i *face = (Vec3i*)malloc(sizeof(Vec3i)*3);
 
-			face[0].x = strtof(ptr, &end); ptr = end+1;
+			face[0].x = strtof(ptr, &end)-1; ptr = end+1;
 			// printf("[DEV]f %ld/", l);
-			face[0].y = strtof(ptr, &end); ptr = end+1;
+			face[0].y = strtof(ptr, &end)-1; ptr = end+1;
 			// printf("%ld/", l);
-			face[0].z = strtof(ptr, &end); ptr = end+1;
+			face[0].z = strtof(ptr, &end)-1; ptr = end+1;
 			// printf("%ld ", l);
 
 
-			face[1].x = strtof(ptr, &end); ptr = end+1;
+			face[1].x = strtof(ptr, &end)-1; ptr = end+1;
 			// printf("%ld/", l);
-			face[1].y = strtof(ptr, &end); ptr = end+1;
+			face[1].y = strtof(ptr, &end)-1; ptr = end+1;
 			// printf("%ld/", l);
-			face[1].z = strtof(ptr, &end); ptr = end+1;
+			face[1].z = strtof(ptr, &end)-1; ptr = end+1;
 			// printf("%ld ", l);
 
 
-			face[2].x = strtof(ptr, &end); ptr = end+1;
+			face[2].x = strtof(ptr, &end)-1; ptr = end+1;
 			// printf("%ld/", l);
-			face[2].y = strtof(ptr, &end); ptr = end+1;
+			face[2].y = strtof(ptr, &end)-1; ptr = end+1;
 			// printf("%ld/", l);
-			face[2].z = strtof(ptr, &end); ptr = end+1;
+			face[2].z = strtof(ptr, &end)-1; ptr = end+1;
 			// printf("%ld\n", l);
+
+            printf("f %d/%d/%d %d/%d/%d %d/%d/%d\n", 
+                    face[0].x, face[0].y, face[0].z,
+                    face[1].x, face[1].y, face[1].z,
+                    face[2].x, face[2].y, face[2].z);
 
 			model->faces[f_index++] = face;
 
@@ -154,7 +159,8 @@ tobj_model* tobj_load_model(const char *filename, bool has_texture) {
 tt_color tobj_diffuse(tobj_model *model, Vec2i uv) {
     assert(model != NULL);
     assert(model->_diffuse_map != NULL);
-    tt_color color = tt_make_color(model->_diffuse_map->pixels[(uv.y-1)*model->_diffuse_map->width+uv.x-1]);
+    // FIX: -1
+    tt_color color = tt_make_color(model->_diffuse_map->pixels[(uv.y)*model->_diffuse_map->width+uv.x]);
     return color;
 }
 
